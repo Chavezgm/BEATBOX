0dacc6f942b16c176fd82f4f970a3bf420d5d0db
Martin Chavez <martinchavez1203@gmail.com>
2023-10-12 18:41:35 -0600

    Instantiates & calls data

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,19 @@
-require ',/lib/node'
+require './lib/node'
+
+RSpec.describe Node do 
+  before (:each) do 
+    @node = Node.new("plop")
+  end
+
+  describe 'instantiates' do 
+    it 'exists' do 
+      expect(@node).to be_instance_of(Node)
+    end
+  end
+
+  describe '#data' do 
+    it 'calls data' do 
+      expect(@node.data).to eq("plop")
+    end
+  end
+end
